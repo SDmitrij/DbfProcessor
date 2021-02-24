cf9a8e4c952b855f47b3f3c56add8d6064e5cf61
Дмитрий Сиделёв <sidelevdima@yandex.ua>
2021-02-24 11:53:29 +0300

    Refactor in progress

---
+++ Core/Exceptions/ExchangeException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DbfProcessor.Core.Exceptions
+{
+    public class ExchangeException : Exception
+    {
+        public ExchangeException(string message) : base(message) { }
+    }
+}
